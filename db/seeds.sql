843cc3585b8625ff7fce2ec716a0d5d0916acfd3
sp <sam.pack.sp@gmail.com>
2020-11-21 17:04:56 +1030

    first commit with file structure and basic set-up referencing bootcamp wk 13 activity 17

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Buttburn');
+INSERT INTO burgers (burger_name) VALUES ('Ninjaface');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Rocky Road', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Gut Buster', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Smelly burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Vegetarian');
---
+++ models/burger.js
@@ -0,0 +1,3 @@
+// Import the ORM to create functions that will interact with the database.
+var orm = require("../config/orm.js");
+
