5bb81ee456080a1d766c0cb6ac1f41a64dfbf6c0
sp <sam.pack.sp@gmail.com>
2020-11-22 12:33:19 +1030

    added heoku add-on jawsdb to connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,18 @@
 // Set up MySQL connection.
-var mysql = require("mysql");
+let mysql = require("mysql");
+let connection;
 
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else {
+ connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "1234",
   database: "burger_db"
 });
-
+};
 // Make connection.
 connection.connect(function(err) {
   if (err) {
